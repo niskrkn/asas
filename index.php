b8cc1b8b9abe85d2bed89d88c0d972d2059c1a62
niskrkn <erdenkajyrbek2@gmaol.com>
2024-03-11 23:15:04 +0600

    пятое задание

--- index.php
+++ index.php
@@ -1,48 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Конвертер температуры</title>
-</head>
-<body>
-
-<h2>Конвертер температуры</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <label for="temperature">Температура:</label>
-    <input type="text" id="temperature" name="temperature" required><br>
-
-    <label for="unit">Выберите единицу измерения:</label>
-    <select id="unit" name="unit" required>
-        <option value="celsius">Цельсий</option>
-        <option value="fahrenheit">Фаренгейт</option>
-    </select><br>
-
-    <input type="submit" name="submit" value="Конвертировать">
-</form>
-
 <?php
-// Функция для конвертации температуры из Цельсия в Фаренгейты
-function celsius_to_fahrenheit($celsius) {
-    return ($celsius * 9/5) + 32;
-}
+// Заданные числа для сравнения
+$number1 = 10;
+$number2 = 20;
+$number3 = 15;
 
-// Функция для конвертации температуры из Фаренгейтов в Цельсии
-function fahrenheit_to_celsius($fahrenheit) {
-    return ($fahrenheit - 32) * 5/9;
-}
+// Находим наибольшее число
+$max_number = max($number1, $number2, $number3);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $temperature = $_POST['temperature'];
-    $unit = $_POST['unit'];
+// Находим наименьшее число
+$min_number = min($number1, $number2, $number3);
 
-    if ($unit == "celsius") {
-        $converted_temperature = celsius_to_fahrenheit($temperature);
-        echo "Температура в градусах Фаренгейта: $converted_temperature °F";
-    } elseif ($unit == "fahrenheit") {
-        $converted_temperature = fahrenheit_to_celsius($temperature);
-        echo "Температура в градусах Цельсия: $converted_temperature °C";
-    }
-}
+// Выводим результат
+echo "Наибольшее число: $max_number<br>";
+echo "Наименьшее число: $min_number";
 ?>
-
-</body>
-</html>
